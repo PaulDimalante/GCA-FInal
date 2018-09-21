476506ad3224661f6ffff7a92ff6880457e90b72
Paul Dimalante <Paul.Dimalante@gmail.com>
2018-09-20 21:24:41 -0400

    junit

---
+++ src/daoTest/TestSuite.java
@@ -0,0 +1,11 @@
+package daoTest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	GCA_MemberDAOTest.class
+})
+public class TestSuite {
+}
